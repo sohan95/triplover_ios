de30868a24f754ee0aae48b985aba2a7628d263d
Sohan <sohansust95@gmail.com>
2022-06-01 16:46:45 +0600

    --Add AirportData

---
+++ flightexpert/Models/Airports/AirportData.swift
@@ -0,0 +1,13 @@
+//
+//  AirportData.swift
+//  Created by sohan on 5/27/22.
+//
+
+import Foundation
+
+struct AirportData: Hashable, Codable {
+    var name: String
+    var city: String
+    var country: String
+    var iata: String
+}
